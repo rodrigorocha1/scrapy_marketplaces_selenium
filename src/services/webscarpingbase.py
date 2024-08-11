1b112c7345d139f8093e560dc248f6178f523e53
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-08-11 16:45:34 -0300

    incluir para registra a data e hora da obtenção do produto

--- src/services/webscarpingbase.py
+++ src/services/webscarpingbase.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from abc import abstractmethod
+from typing import (Generator, Tuple)s
 
 
 class WebScrapingBase:
@@ -12,25 +13,54 @@ class WebScrapingBase:
         self.navegador = webdriver.Chrome(service=self.__servico)
         self.navegador.maximize_window()
 
-    def abrir_navegador(self, url: str) -> WebDriver:
+    def abrir_navegador(self, url: str):
+        """Método para abrir o navegador e conectar na url
+
+        Args:
+            url (str): url do site
+
+        """
         self.navegador.get(url)
 
     @abstractmethod
     def fazer_pesquisa_produto(self, termo_busca: str) -> None:
+        """Método para fazer a pesquisa de um produto
+
+        Args:
+            termo_busca (str): Nome do produto: Ex: Churrasqueira
+        """
         pass
 
     @abstractmethod
     def clicar_botao_pesquisa(self) -> None:
+        """Método para fazer a pesquisa
+        """
         pass
 
     @abstractmethod
     def selecionar_faixa_preco(self, preco_menor: float, preco_maior: float):
+        """Método para selecionar a faixa de preço
+
+        Args:
+            preco_menor (float): preço menor do produto
+            preco_maior (float): preço maior do produto
+        """
         pass
 
     @abstractmethod
-    def coletar_dados_produtos(self):
+    def coletar_dados_produtos(self) -> Generator[Tuple[str, str, str, str, str], None, None]:
+        """Método para retornar os dados de cada produto por vez
+
+        Yields:
+            Generator[Tuple[str, str, str, str, str], None, None]: Um gerador de produtos
+        """
         pass
 
     @abstractmethod
     def executar_paginacao(self) -> bool:
+        """Execcuta a páginação
+
+        Returns:
+            bool: Verdadeiro caso a páginação seja feita com sucesso, falso caso contrário
+        """
         pass
--- src/services/webscrapingleroymerlin.py
+++ src/services/webscrapingleroymerlin.py
@@ -2,31 +2,61 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from src.services.webscarpingbase import WebScrapingBase
+from typing import (Generator, Tuple)
+from datetime import datetime
 
 
 class WebScrapingLeroyMerling(WebScrapingBase):
 
     def __init__(self) -> None:
+        self.__data_extracao = self.__data_atual()
         super().__init__()
 
+    def __data_atual(self) -> datetime:
+        """Método para obter a data atual
+
+        Returns:
+            datetime: data_atual
+        """
+        data_atual = datetime.now()
+        data_formatada = data_atual.strftime('%Y-%m-%d %H:%M:%S')
+        data_datetime = datetime.strptime(data_formatada, '%Y-%m-%d %H:%M:%S')
+        return data_datetime
+
     def fazer_pesquisa_produto(self, termo_busca: str) -> None:
+        """Método para abrir o navegador e conectar na url
+
+        Args:
+            url (str): url do site
+
+        """
         busca_produto = self.navegador.find_element(
             By.ID, '@autocomplete-quicksearch-input')
         busca_produto.send_keys(termo_busca)
 
     def __esperar_elemento(self):
+        """Espera a tag do css antes de fazer as pesquisas
+        """
         WebDriverWait(self.navegador, 20).until(
             EC.presence_of_element_located(
                 (By.CSS_SELECTOR,
                  '#mobile-filter-content > div > div > div:nth-child(6) > div.flex-1.px-4.py-4 > div > div:nth-child(1) > div > input')))
 
     def clicar_botao_pesquisa(self) -> None:
+        """Método para fazer a pesquisa
+        """
         self.navegador.find_element(
             By.XPATH,
             '/html/body/header/div[2]/div/div[1]/form/div/div[1]/div/div/button'
         ).click()
 
     def selecionar_faixa_preco(self, preco_menor: float, preco_maior: float):
+        """Método para selecionar a faixa de preço
+
+        Args:
+            preco_menor (float): preço menor do produto
+            preco_maior (float): preço maior do produto
+        """
         self.__esperar_elemento()
         preco_minimo = self.navegador.find_element(
             By.CSS_SELECTOR,
@@ -42,7 +72,20 @@ class WebScrapingLeroyMerling(WebScrapingBase):
             '//*[@id="mobile-filter-content"]/div/div/div[6]/div[2]/div/button'
         ).click()
 
-    def coletar_dados_produtos(self):
+    def __executar_rolagem(self, chave: int):
+        """Executa a barra de rolagem
+
+        Args:
+            chave (int): número do loop
+        """
+        self.navegador.execute_script(f'window.scroll(0, {chave * 80})')
+
+    def coletar_dados_produtos(self) -> Generator[Tuple[str, str, str, str, str], None, None]:
+        """Método para retornar os dados de cada produto por vez
+
+        Yields:
+            Generator[Tuple[str, str, str, str, str], None, None]: Um gerador de produtos
+        """
         lista_produtos = self.navegador.find_elements(
             By.CLASS_NAME,  'new-product-thumb')
         for chave, produto in enumerate(lista_produtos):
@@ -56,9 +99,15 @@ class WebScrapingLeroyMerling(WebScrapingBase):
                 By.CLASS_NAME, 'css-1n5vdld-product-thumbnail__image').get_attribute('src')
             url_produto = produto.find_element(
                 By.TAG_NAME, 'a').get_attribute('href')
-            self.navegador.execute_script(f'window.scroll(0, {chave * 80})')
+            yield nome, codigo, preco, url_imagem, url_produto, self.__data_atual
+            self.__executar_rolagem(chave=chave)
 
     def executar_paginacao(self) -> bool:
+        """Execcuta a páginação
+
+        Returns:
+            bool: Verdadeiro caso a páginação seja feita com sucesso, falso caso contrário
+        """
         try:
             self.navegador.find_element(
                 By.CLASS_NAME, 'glyph-arrow-right').click()
